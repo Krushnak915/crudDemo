91d269c6fbd5372d7ebf107e21661d8c534065d5
krushna's PC <krushna's PC@Krushna>
2023-08-08 22:24:08 -0700

    c1

---
+++ CrudDemo/CrudDemo/src/main/java/com/demo/CrudDemoApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+		System.out.println("program started.....jayho!");
+	}
+
+}
